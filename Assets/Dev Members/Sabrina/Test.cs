cbbcd53107baffdf532b3c4c2d28f9255a92d317
Sab-Kay <sonic13562@gmail.com>
2023-09-09 16:10:43 +1000

    Just Testing Commits

---
+++ Assets/Dev Members/Sabrina/Test.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            Debug.Log("Hello!");
+        }
+    }
+}
