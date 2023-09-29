c2c1ef43007f1210b1c5e3a15f11d3c06f66ba44
Roberto <robbie.murgado@gmail.com>
2023-09-29 10:27:49 -0700

    Cutscene Director Script

---
+++ Assets/Scripts/Cutscenes/Cutscene Actions/BaseCutsceneAction.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu]
+public class BaseCutsceneAction : ScriptableObject
+{
+    public new string name;
+    public string type;
+
+    //"direction" Action
+    [TextArea(3, 10)]
+    public string[] sentences;
+    //"move" Action
+    public Vector3 destinationPos;
+}
