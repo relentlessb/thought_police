701f7b1ee975097d4144daa63c5855de8ecc6f87
yesterspectre <yesterspectre@gmail.com>
2023-09-30 19:37:58 -0700

    Add prototype for general status effects, adjust player and enemy
    Adds some stats to players and enemies, adds basic logic for adding effects to entities.
    
    Is currently broken in that effect is not instantiated properly.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,5 +1,8 @@
+using System;
+using System.CodeDom;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using Unity.VisualScripting;
 using Unity.VisualScripting.Antlr3.Runtime.Misc;
@@ -8,49 +11,75 @@ using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
-    public Dictionary<string, float> currentStats;
+    // scene variables
     [SerializeField] SceneHandler sceneHandler;
     public Dictionary<(int, int), string> sceneMap;
-    public List<GameObject> doorList;
-    public List<((int, int), (int, int))> doorDictionary;
-    [SerializeField] UnityEvent<List<((int, int), (int, int))>, Dictionary<(int, int), string>, (int, int), List<GameObject>> touchedDoor;
-    public (int, int) currentPos;
-    [SerializeField] Camera cameraObject;
-    public int doorEnterDistance;
-    SpriteRenderer objectSprite;
-    int spriteTimer = 0;
-    Camera MainCamera;
-    [SerializeField] UnityEvent<Dictionary<string, float>, Sprite, Player> startCharSwitch;
-    Rigidbody2D playerPhys;
-    public List<Dictionary<string, float>> charStats;
-    public List<Dictionary<string, float>> charStatsBase;
-    public List<Sprite> charSprites;
-    [SerializeField] GameObject weapon;
+
+    // UI variables
     [SerializeField] UIMapHandler map;
     UIMapHandler mapObj;
-    GameObject straightSword;
-    [SerializeField] GameObject weaponAngleObject;
-    GameObject weaponAngleObj;
-    public List<BaseAbility> passiveAbilitiesPathos= new List<BaseAbility>();
+
+    // camera variables
+    [SerializeField] Camera cameraObject;
+    Camera MainCamera;
+
+    // door variables
+    public List<GameObject> doorList;
+    public List<((int, int), (int, int))> doorDictionary;
+    public int doorEnterDistance;
+    [SerializeField] UnityEvent<List<((int, int), (int, int))>, Dictionary<(int, int), string>, (int, int), List<GameObject>> touchedDoor;
+
+    // player stat variables
+    public Dictionary<string, float> currentStats;
+    public List<Dictionary<string, float>> charStats;
+    public List<Dictionary<string, float>> charStatsBase;
+
+    // player ability variables
+    public List<BaseAbility> passiveAbilitiesPathos = new List<BaseAbility>();
     public List<BaseAbility> passiveAbilitiesEthos = new List<BaseAbility>();
     public List<BaseAbility> passiveAbilitiesLogos = new List<BaseAbility>();
     public List<List<BaseAbility>> passiveAbilities;
     int passiveNum;
-    [SerializeField] ActiveHolder activeHolder;
+
+    // player active effect variables
+    public List<EffectHolder> statusEffects = new List<EffectHolder>();  // Array of status effects currently attached to the player
+    public List<EffectHolder> weaponEffects = new List<EffectHolder>();  // Array of weapon effects currently attached to the player
+    int statusNum;
+
+    // equipment variables
+    [SerializeField] GameObject weapon;
+    [SerializeField] GameObject weaponAngleObject;
+    GameObject weaponAngleObj;
+    GameObject straightSword;
+
+    // position variables
+    public (int, int) currentPos;
+
+    // physics variables
     public bool canMove = true;
+    Rigidbody2D playerPhys;
+
+    // graphics variables
+    SpriteRenderer objectSprite;
+    int spriteTimer = 0;
+    public List<Sprite> charSprites;
+    [SerializeField] UnityEvent<Dictionary<string, float>, Sprite, Player> startCharSwitch;
+
+    [SerializeField] ActiveHolder activeHolder;
+
 
     private void Start()
     {
         playerPhys = gameObject.GetComponent<Rigidbody2D>();
         MainCamera = Instantiate(cameraObject, transform.parent = this.transform);
         weaponAngleObj = Instantiate(weaponAngleObject, transform.parent = this.transform);
-        mapObj = Instantiate(map, transform.parent=this.transform);
+        mapObj = Instantiate(map, transform.parent = this.transform);
         mapObj.sceneMap = sceneMap;
         mapObj.currentPos = currentPos;
         mapObj.updateMap = true;
         straightSword = Instantiate(weapon, transform.parent = weaponAngleObj.transform);
         objectSprite = gameObject.GetComponent<SpriteRenderer>();
-        passiveAbilities = new List<List<BaseAbility>> 
+        passiveAbilities = new List<List<BaseAbility>>
         {passiveAbilitiesPathos, passiveAbilitiesEthos, passiveAbilitiesLogos};
         currentStats = new Dictionary<string, float>()
         {
@@ -66,7 +95,9 @@ public class Player : MonoBehaviour
             { "Speed", 1 }
         };
 
-}
+        // TODO-Deviant: test code for equipping and adding effect
+        weapon.GetComponent<BaseSwordScript>().onEquip(this);
+    }
     public int currentChar = 0;
 
     private void Update()
@@ -78,7 +109,7 @@ public class Player : MonoBehaviour
             playerPhys.velocity = playerMovement;
         }
         //Reload Sprite and Camera after Scene Change
-        if(objectSprite.enabled == false)
+        if (objectSprite.enabled == false)
         {
             spriteTimer += 1;
             if (spriteTimer > 30)
@@ -99,12 +130,23 @@ public class Player : MonoBehaviour
             }
             startCharSwitch.Invoke(charStats[currentChar], charSprites[currentChar], this);
         }
-        if(passiveAbilities[0].Count+passiveAbilities[1].Count+passiveAbilities[2].Count != passiveNum)
+        //UnityEngine.Debug.Log("got here1");
+        if (passiveAbilities[0].Count + passiveAbilities[1].Count + passiveAbilities[2].Count != passiveNum )
         {
             recalculateStats(charStatsBase, passiveAbilities, charStats);
             currentStats = charStats[currentChar];
             passiveNum = passiveAbilities[0].Count + passiveAbilities[1].Count + passiveAbilities[2].Count;
         }
+        recalculateStats(charStatsBase, passiveAbilities, charStats);
+        // check for application/expiry of active effects
+        // Effects can be registered for two? reasons:
+        //   1. Player is afflicted with a status (good or bad)
+        //   2. Player swaps weapons
+
+        // Effects can expire for two? reasons?
+        //   1. Timer for status has expired
+        //   2. Player swaps weapons
+
     }
     void OnTriggerEnter2D(UnityEngine.Collider2D other)
     {
@@ -112,23 +154,24 @@ public class Player : MonoBehaviour
         {
             case "Ability":
                 {
-                    if(other.GetComponent<AbilityHolder>().heldAbility!=null)
+                    if (other.GetComponent<AbilityHolder>().heldAbility != null)
                     {
                         BaseAbility ability = other.GetComponent<AbilityHolder>().heldAbility;
                         string type = ability.type;
                         switch (type)
                         {
-                            case "Passive": if (!passiveAbilities[currentChar].Contains(ability)) 
-                                { 
+                            case "Passive":
+                                if (!passiveAbilities[currentChar].Contains(ability))
+                                {
                                     passiveAbilities[currentChar].Add(ability);
-                                    other.GetComponent<AbilityHolder>().heldAbility = null; 
-                                }  
+                                    other.GetComponent<AbilityHolder>().heldAbility = null;
+                                }
                                 break;
-                            case "Active": 
-                                if (activeHolder.ability == null) 
-                                { 
+                            case "Active":
+                                if (activeHolder.ability == null)
+                                {
                                     activeHolder.ability = ability;
-                                } 
+                                }
                                 break;
                         }
                     }
@@ -140,8 +183,8 @@ public class Player : MonoBehaviour
                     switch (other.gameObject.name)
                     {
                         case "North Door": currentPos = (currentPos.Item1, currentPos.Item2 + 1); break;
-                        case "South Door": currentPos = (currentPos.Item1, currentPos.Item2 - 1);  break;
-                        case "East Door": currentPos = (currentPos.Item1 - 1, currentPos.Item2);  break;
+                        case "South Door": currentPos = (currentPos.Item1, currentPos.Item2 - 1); break;
+                        case "East Door": currentPos = (currentPos.Item1 - 1, currentPos.Item2); break;
                         case "West Door": currentPos = (currentPos.Item1 + 1, currentPos.Item2); break;
                     }
                     mapObj.currentPos = currentPos;
@@ -156,24 +199,50 @@ public class Player : MonoBehaviour
                     }
                     MainCamera.enabled = false;
                     objectSprite.enabled = false;
-                    transform.position= newPosition;
+                    transform.position = newPosition;
                     break;
                 }
         }
     }
-    public void recalculateStats(List<Dictionary<string,float>> charStatsBase, List<List<BaseAbility>> passiveAbilities, List<Dictionary<string, float>> charStats)
+
+    // recalculates stats for all characters on the fly
+    public void recalculateStats(List<Dictionary<string, float>> charStatsBase, List<List<BaseAbility>> passiveAbilities, List<Dictionary<string, float>> charStats)
     {
-        for(int i = 0; i<=2; i++)
+        // loop through all 3
+        for (int i = 0; i <= 2; i++)
         {
+            // grab the base stats
             charStats = charStatsBase;
-            foreach(string statName in charStats[i].Keys.ToList())
+
+            // for each stat, cycle through all passive abilities, active effects, and weapon effects
+            foreach (string statName in charStats[i].Keys.ToList())
             {
-                foreach(BaseAbility ability in passiveAbilities[i])
+                foreach (BaseAbility ability in passiveAbilities[i])
                 {
                     charStats[i][statName] += ability.statChange[statName];
                 }
-                
+                // apply status effects
+                foreach (EffectHolder effectHolder in statusEffects)
+                {
+                    charStats[i][statName] += effectHolder.effect.statChange[statName];
+                }
+
+                //TODO-Deviant: copy from status above
+
             }
         }
+    }
+
+    public void registerStatus(EffectHolder caller)
+    {
+        statusEffects.Add(caller);
+        caller.listIndex = statusEffects.Count;
+        statusNum++;
+    }
+
+    public void removeStatus(EffectHolder caller)
+    {
+        statusEffects.RemoveAt(caller.listIndex);
+        statusNum--;
     }
 }
--- Assets/Scripts/Weapons/BaseSwordScript.cs
+++ Assets/Scripts/Weapons/BaseSwordScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -7,6 +8,15 @@ public class BaseSwordScript : MonoBehaviour
 {
     Animator animator;
     SpriteRenderer spriteVis;
+    [SerializeField] EffectHolder attackEffectHolder;  // this is the effect of attacks dealt by this weapon
+    [SerializeField] EffectHolder holdingEffectHolder; // this is the effect of holding the weapon (on the holder)
+
+    public void onEquip(Player player)
+    {
+        player.registerStatus(holdingEffectHolder);
+    }
+
+
     private void Start()
     {
         animator = GetComponent<Animator>();
@@ -23,8 +33,14 @@ public class BaseSwordScript : MonoBehaviour
             animator.SetBool("swordSwing", false);
         }
     }
-    public void hitEnemy(GameObject enemyCollider)
+    public void hitEnemy(GameObject enemyCollider, BaseEnemyScript enemyObject)
     {
-
+        // TODO: apply damage
+        // TODO: apply weapon effect based on proc percentage
+        if (UnityEngine.Random.Range(0f, 1.0f) <= attackEffectHolder.effect.hitPercentage)
+        {
+            // add the effect to the enemy
+            enemyObject.effects.Add(attackEffectHolder);
+        }
     }
 }
